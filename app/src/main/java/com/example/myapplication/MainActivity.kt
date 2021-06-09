9eef7a7c06d574b757567c083433d71a40f2fe92
Sandro <sandro.shanshiashvili.1@btu.edu.ge>
2021-06-09 19:24:05 +0400

    ini

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.adapter.UserAdapter
+import com.example.myapplication.api.RetrofitClient
+import com.example.myapplication.api.dto.ReqresData
+import com.example.myapplication.api.dto.User
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainActivity : AppCompatActivity() {
+    var dataset : List<User>? = listOf()
+    lateinit var recyclerView : RecyclerView
+    var adapter = UserAdapter()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recycler)
+
+
+        RetrofitClient.reqResApi.getUsers(2).enqueue(object : Callback<ReqresData<List<User>>> {
+                override fun onResponse(
+                    call: Call<ReqresData<List<User>>>,
+                    response: Response<ReqresData<List<User>>>
+                ) {
+                    if (response.isSuccessful && response.body() != null) {
+                        dataset = response.body()?.data
+                        adapter.setData(dataset!!)
+                        response.body()!!.data?.forEach { i -> Log.d("Users", i.toString()) }
+                    }
+                }
+
+                override fun onFailure(call: Call<ReqresData<List<User>>>, t: Throwable) {
+
+                }
+
+            })
+
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+
+    }
+}
